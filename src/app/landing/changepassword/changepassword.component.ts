c5666f6b7b89e1d7857940bf7adaf36c1c72368c
Jonathan Ospina Garcia <jonathan.ospina@bios.co>
2021-08-25 09:50:28 -0500

    Se anade la biblioteca de bodymovin para animar el logo de bios y se mejora el responsive

--- src/app/landing/changepassword/changepassword.component.ts
+++ src/app/landing/changepassword/changepassword.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { NgwWowService } from 'ngx-wow';
+import * as Vivus from 'vivus';
+import * as bodymovin from "bodymovin";
+import lottie from "lottie-web";
 declare var $: any;
 
 @Component({
@@ -11,9 +16,40 @@ export class ChangepasswordComponent implements OnInit {
 
   cargando = false;
 
-  constructor() { }
+  windowScrolled: boolean;
+  Vivus: any;
+
+  constructor(private wowService: NgwWowService, @Inject(DOCUMENT) private document: Document) {
+    this.wowService.init();
+   }
+
+  @HostListener("window:scroll")
+  onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+        this.windowScrolled = true;
+      }
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
 
   ngOnInit(): void {
+    this.initbodymovin();
+    this.wowService.init();
+  }
+
+  initbodymovin() {
+    lottie.loadAnimation({
+      container:document.getElementById('logoBiosAnimated'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets10.lottiefiles.com/packages/lf20_tnczcwop/Logo/data.json'
+    })
+  }
+
+  reset() {
+    this.wowService.init();
   }
 
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { NgwWowService } from 'ngx-wow';
+import * as Vivus from 'vivus';
+import * as bodymovin from "bodymovin";
+import lottie from "lottie-web";
 declare var $: any;
 
 @Component({
@@ -12,12 +16,36 @@ export class LandingComponent implements OnInit {
 
   cargando = false;
 
-  constructor(private wowService: NgwWowService,) {
+  windowScrolled: boolean;
+  Vivus: any;
+
+  constructor(private wowService: NgwWowService, @Inject(DOCUMENT) private document: Document) { 
     this.wowService.init();
   }
 
+  @HostListener("window:scroll")
+  onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+        this.windowScrolled = true;
+      }
+     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+      }
+  }
+
   ngOnInit(): void {
+    this.initbodymovin();
     this.wowService.init();
+  }
+
+  initbodymovin() {
+    lottie.loadAnimation({
+      container:document.getElementById('logoBiosAnimated'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets10.lottiefiles.com/packages/lf20_tnczcwop/Logo/data.json'
+    })
   }
 
   reset() {
